9a33011a6e0b3cda4e4152b6ce037c1daef76d41
Denis Chaimow <dchaimow@users.noreply.github.com>
2021-06-30 12:53:03 +0200

    bugfix

--- asrs_mp2rage.py
+++ asrs_mp2rage.py
@@ -17,16 +17,6 @@ def generate_mp2rage_refs(inv2_ses1, uni_ses1,inv2_ses2, uni_ses2):
     ref2=maskuni2_result.outputs.out_file
     return ref1, ref2
 
-
-def asrs_mp2rage(slab1, inv2_ses1, uni_ses1,inv2_ses2, uni_ses2):
-    reg1, reg2 = generate_mp2rage_refs(inv2_ses1, uni_ses1,inv2_ses2, uni_ses2)
-    xform = asrs.registerOldSlabToNewRef(slab1,ref1,ref2)
-    img_slab1 = nb.load(slab1)
-    img_ref2 = nb.load(ref2)
-    dims=img_slab1.shape[:3]
-    sform = flirtToSform(xform,img_slab1,img_ref2) 
-    qform2SiemensProtocol(sform,dims)
-
 if __name__ == "__main__":
     if len(sys.argv)==7:
         dicomExportPath = sys.argv[1]
@@ -35,8 +25,8 @@ if __name__ == "__main__":
         inv2_ses1 = sys.argv[4]    
         uni_ses1 = sys.argv[5]
         slab1 = sys.argv[6]
-        inv2_ses2 = loadFromDicomExport(dicomExportPath, seriesNumberINV2)
-        uni_ses2 = loadFromDicomExport(dicomExportPath, seriesNumberUNI)
+        inv2_ses2 = asrs.loadFromDicomExport(dicomExportPath, seriesNumberINV2)
+        uni_ses2 = asrs.loadFromDicomExport(dicomExportPath, seriesNumberUNI)
     elif len(sys.argv)==8:
         dicomExportPathINV2 = sys.argv[1]
         seriesNumberINV2 = sys.argv[2]
@@ -45,10 +35,11 @@ if __name__ == "__main__":
         inv2_ses1 = sys.argv[5]    
         uni_ses1 = sys.argv[6]
         slab1 = sys.argv[7]
-        inv2_ses2 = loadFromDicomExport(dicomExportPathINV2, seriesNumberINV2)
-        uni_ses2 = loadFromDicomExport(dicomExportPathUNI, seriesNumberUNI)
+        inv2_ses2 = asrs.loadFromDicomExport(dicomExportPathINV2, seriesNumberINV2)
+        uni_ses2 = asrs.loadFromDicomExport(dicomExportPathUNI, seriesNumberUNI)
     else:
         print('Usage: asrs_mp2rage.py dicomExportPath seriesNumberINV2 seriesNumberUNI inv2_ses1 uni_ses1 slab1')
         print('or: asrs_mp2rage.py dicomExportPathINV2 seriesNumberINV2 dicomExportPathUNI seriesNumberUNI inv2_ses1 uni_ses1 slab1')
         exit
-    asrs_mp2rage(slab1, inv2_ses1, uni_ses1, inv2_ses2, uni_ses2)
+    ref1, ref2 = generate_mp2rage_refs(inv2_ses1, uni_ses1,inv2_ses2, uni_ses2)
+    asrs.asrs(slab1, ref1, ref2)
